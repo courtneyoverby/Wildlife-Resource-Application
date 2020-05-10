5fe0dbf0390d812193650a6f36377ee7731f4240
Courtney Overby <brioverby@gmail.com>
2020-05-10 17:33:55 -0500

    updates route for resources nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -31,7 +31,7 @@ const Nav = (props) => {
         {/* Show the link to the info page and the logout button if the user is logged in */}
         {props.store.user.id && (
           <>
-            <Link className="nav-link" to="/info">
+            <Link className="nav-link" to="/resources">
               Resources
             </Link>
             <LogOutButton className="nav-link" />
