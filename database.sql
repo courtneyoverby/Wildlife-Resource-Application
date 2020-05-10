cdc72ac87021846cc1256f7cf35624bc5746afe1
Courtney Overby <brioverby@gmail.com>
2020-05-10 17:27:48 -0500

    Revert "updated database.sql"
    This reverts commit 656b95826b979c7968cd19a056be9ade4d56e6ef.

--- database.sql
+++ database.sql
@@ -8,16 +8,3 @@ CREATE TABLE "user" (
     "username" VARCHAR (80) UNIQUE NOT NULL,
     "password" VARCHAR (1000) NOT NULL
 );
-
-CREATE TABLE "resources" (
-	"id" SERIAL PRIMARY KEY,
-	"name" VARCHAR (80) NOT NULL,
-	"hours" VARCHAR (150) NOT NULL,
-	"number" VARCHAR (15) NOT NULL,
-	"address" VARCHAR (150) NOT NULL,
-    "information" VARCHAR (1000)
-	);
-
-    INSERT INTO "resources" ("name",  "hours", "number", "address", "information")
-	VALUES ('Wildlife Rescue Center', 'Mon. - Fri. 9AM-5PM, Sat. - Sun. 9AM-3PM', '(636) 394-1880', '1128 New Ballwin Rd, Ballwin, MO 63021', "Rescuing, Rehabilitating, Releasing Native Wildlife Since 1979");
-
