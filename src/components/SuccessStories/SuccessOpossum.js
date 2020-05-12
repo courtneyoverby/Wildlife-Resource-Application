a77b056bb16aa44406f49ea2a212f15fd28d184c
Courtney Overby <brioverby@gmail.com>
2020-05-12 13:59:57 -0500

    added opossum success story and image

--- src/components/SuccessStories/SuccessOpossum.js
+++ src/components/SuccessStories/SuccessOpossum.js
@@ -9,6 +9,24 @@ class SuccessOpossum extends Component {
 
     return (
       <div>
+        <h4>Orphaned Opossums Get A Fostered</h4>
+        <img alt="Opossums" src="../images/WR3.jpg"></img>
+        <p>
+          All Things Wild Rehabilitation in Texas were introduced to the baby
+          opossums when their mother was hit and killed by a car. Named Sting
+          and Trudy after the British musician, they seemed to have no trouble
+          growing, continuing to do so until they were lapping formula and
+          starting to wean. Then, Sting continued growing, but Trudy stopped
+          thriving.
+        </p>
+        <p>
+          Sting grew to be almost 5 times the size of Trudy, while Trudy
+          continued to lose ground rapidly. Fortunately, they rehabilitation was
+          able to entice Trudy to lap up some meal replacements. After only a
+          couple weeks, Trudy was back to thriving. Even though she will never
+          grow to the size of her pouch-mate, Sting, she will be released back
+          into the wild with him.
+        </p>
         <button onClick={handleClick("/admin")}>Go Back</button>
       </div>
     );
