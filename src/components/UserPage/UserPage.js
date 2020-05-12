1191bb55af8447c52c776731774618bc05c4625d
Courtney Overby <brioverby@gmail.com>
2020-05-12 10:46:26 -0500

    set up links from userpage/homepage to success stories

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -2,25 +2,38 @@ import React from "react";
 import { connect } from "react-redux";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import mapStoreToProps from "../../redux/mapStoreToProps";
-import { retry } from "redux-saga/effects";
+import { withRouter } from "react-router-dom";
+// import mapStoreToProps from "../../redux/mapStoreToProps";
 
 // this could also be written with destructuring parameters as:
 // const UserPage = ({ user }) => (
 
 const UserPage = (props) => {
-  const handleClick = (event) => {
-    this.props.history.push(`/owl-success`);
+  const handleClick = (path) => (event) => {
+    props.history.push(path);
   };
   return (
     <div>
       <h1 id="welcome">Welcome, {props.store.user.username}!</h1>
       <p>
         Click on a photo below to read more about the wildlife rehabilitation
-        has saved!
+        has saved! Or use the navigation bar to go to the resource list!
       </p>
-      <img onClick={handleClick} alt="Owl" src="../images/WR1.jpg"></img>
-      <img alt="Porcupine" src="../images/WR2.jpg"></img>
-      <img alt="Opossums" src="../images/WR3.jpg"></img>
+      <img
+        onClick={handleClick("/owl-success")}
+        alt="Owl"
+        src="../images/WR1.jpg"
+      ></img>
+      <img
+        onClick={handleClick("/porcupine-success")}
+        alt="Porcupine"
+        src="../images/WR2.jpg"
+      ></img>
+      <img
+        onClick={handleClick("/opossum-success")}
+        alt="Opossums"
+        src="../images/WR3.jpg"
+      ></img>
 
       <LogOutButton className="log-in" />
     </div>
@@ -28,4 +41,4 @@ const UserPage = (props) => {
 };
 
 // this allows us to use <App /> in index.js
-export default connect(mapStoreToProps)(UserPage);
+export default withRouter(connect(mapStoreToProps)(UserPage));
