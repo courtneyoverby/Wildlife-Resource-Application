7ec1ce3b1e4322562b73d745cbf478a2be99bbb9
Courtney Overby <brioverby@gmail.com>
2020-05-12 14:52:36 -0500

    cancel button set up and functioning

--- src/components/ResourcesModal/ResourcesModal.js
+++ src/components/ResourcesModal/ResourcesModal.js
@@ -3,76 +3,23 @@ import { connect } from "react-redux";
 
 class ResourcesModal extends Component {
   state = {
-    updatedResource: {
-      id: "",
-      name: "",
-      hours: "",
-      number: "",
-      address: "",
-      information: "",
-    },
+    name: "",
+    hours: "",
+    number: "",
+    address: "",
+    information: "",
   };
 
   componentDidMount() {
-    this.props.dispatch({ type: "FETCH_RESOURCES" });
+    // dispatch to saga to call server API
+    this.props.dispatch({
+      type: "FETCH_RESOURCES",
+      payload: this.props.match.params.id,
+    });
   }
 
-  handleBtn = (name) => (event) => {
-    if (name === "cancel") {
-      this.props.history.push(`/details/${this.props.match.params.id}`);
-    }
-    if (name === "save") {
-      alert("Information Saved!");
-      this.props.dispatch({
-        type: "FETCH_RESOURCES",
-        payload: this.state.updatedResource,
-      });
-    }
-  };
-
-  handleChangeName = (event) => {
-    this.setState({
-      updatedResource: {
-        ...this.state.updatedResource,
-        name: event.target.value,
-      },
-    });
-  };
-
-  handleChangeHours = (event) => {
-    this.setState({
-      updatedResource: {
-        ...this.state.updatedResource,
-        hours: event.target.value,
-      },
-    });
-  };
-
-  handleChangeNumber = (event) => {
-    this.setState({
-      updatedResource: {
-        ...this.state.updatedResource,
-        number: event.target.value,
-      },
-    });
-  };
-
-  handleChangeAddress = (event) => {
-    this.setState({
-      updatedResource: {
-        ...this.state.updatedResource,
-        address: event.target.value,
-      },
-    });
-  };
-
-  handleChangeInformation = (event) => {
-    this.setState({
-      updatedResource: {
-        ...this.state.updatedResource,
-        information: event.target.value,
-      },
-    });
+  clickCancel = (event) => {
+    this.props.history.push(`/details/${this.props.match.params.id}`);
   };
 
   render() {
@@ -81,41 +28,19 @@ class ResourcesModal extends Component {
         <h3>Edit This Resource</h3>
         <br />
         <div>
-          <button className="editBtn" onClick={this.handleBtn("cancel")}>
+          <button className="cancelBtn" onClick={this.clickCancel}>
             Cancel
           </button>
 
-          <button className="editBtn" onClick={this.handleBtn("save")}>
-            Save
-          </button>
+          <button className="editBtn">Save</button>
         </div>
         <br />
 
-        <input
-          placeholder="Name"
-          type="text"
-          onChange={this.handleChangeName}
-        />
-        <input
-          placeholder="Hours"
-          type="text"
-          onChange={this.handleChangeHours}
-        />
-        <input
-          placeholder="Phone Number"
-          type="text"
-          onChange={this.handleChangeNumber}
-        />
-        <input
-          placeholder="Address"
-          type="text"
-          onChange={this.handleChangeAddress}
-        />
-        <input
-          placeholder="Information"
-          type="text"
-          onChange={this.handleChangeInformation}
-        />
+        <input value="" type="text" />
+        <input placeholder="Hours" type="text" />
+        <input placeholder="Phone Number" type="text" />
+        <input placeholder="Address" type="text" />
+        <input placeholder="Information" type="text" />
       </div>
     );
   }
