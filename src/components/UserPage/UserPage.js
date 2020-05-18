dda0a319c265d25b7865d29b68e0cf2d2014298a
Courtney Overby <brioverby@gmail.com>
2020-05-17 21:01:16 -0500

    fixed onClick for success stories

--- src/components/UserPage/UserPage.js
+++ src/components/UserPage/UserPage.js
@@ -1,6 +1,5 @@
 import React from "react";
 import { connect } from "react-redux";
-import LogOutButton from "../LogOutButton/LogOutButton";
 import mapStoreToProps from "../../redux/mapStoreToProps";
 import { withRouter } from "react-router-dom";
 import "./UserPage.css";
@@ -15,56 +14,55 @@ const UserPage = (props) => {
 
   return (
     <div>
-      <h1 className="welcome-user" id="welcome">
-        Welcome, {props.store.user.username}!
-      </h1>
-      <p className="head-desc">
-        Click on a photo below to read more about the wildlife rehabilitation
-        has saved!
-      </p>
-      <p className="head-desc">
-        Or use the navigation bar to go to the resource list!
-      </p>
-      <div className="grid">
-        <figure className="images">
-          <img
-            className="anml-pix"
-            onClick={handleClick("/owl-success")}
-            alt="Owl"
-            src="../images/WR1.jpg"
-          ></img>
-          <figcaption>
-            <h2>Barred Owl Hit By A Car,</h2>
-            <p> Recovers From Injuries</p>
-            <a href="#"></a>
-          </figcaption>
-        </figure>
-        <figure className="images">
-          <img
-            className="anml-pix"
+      <div className="container bg-img-user">
+        <h1 className="welcome-user" id="welcome">
+          Welcome, {props.store.user.username}!
+        </h1>
+
+        <p className="head-desc">
+          Click on a photo below to read more about the wildlife rehabilitation
+          has saved!
+        </p>
+        <p className="head-desc">
+          Or use the navigation bar to go to the resource list!
+        </p>
+        <div className="grid">
+          <figure onClick={handleClick("/owl-success")} className="images">
+            <img className="anml-pix" alt="Owl" src="../images/WR1.jpg"></img>
+            <figcaption>
+              <h2>Barred Owl</h2>
+              <p> Hit By A Car, Recovers From Injuries</p>
+              <b href="%"></b>
+            </figcaption>
+          </figure>
+          <figure
+            className="images"
             onClick={handleClick("/porcupine-success")}
-            alt="Porcupine"
-            src="../images/WR2.jpg"
-          ></img>
-          <figcaption>
-            <h2>A Porcupine With Mange</h2>
-            <p> Treated And Set Free.</p>
-            <a href="#"></a>
-          </figcaption>
-        </figure>
-        <figure className="images">
-          <img
-            className="anml-pix"
-            onClick={handleClick("/opossum-success")}
-            alt="Opossums"
-            src="../images/WR3.jpg"
-          ></img>
-          <figcaption>
-            <h2>Orphaned Opossums Get A Foster Family,</h2>
-            <p>Recover And Get Released!!</p>
-            <a href="#"></a>
-          </figcaption>
-        </figure>
+          >
+            <img
+              className="anml-pix"
+              alt="Porcupine"
+              src="../images/WR2.jpg"
+            ></img>
+            <figcaption>
+              <h2>A Porcupine</h2>
+              <p> With Mange Treated And Set Free.</p>
+              <b href="%"></b>
+            </figcaption>
+          </figure>
+          <figure onClick={handleClick("/opossum-success")} className="images">
+            <img
+              className="anml-pix"
+              alt="Opossums"
+              src="../images/WR3.jpg"
+            ></img>
+            <figcaption>
+              <h2>Orphaned Opossums</h2>
+              <p> Get A Foster Family,Recover And Get Released!</p>
+              <b href="%"></b>
+            </figcaption>
+          </figure>
+        </div>
       </div>
     </div>
   );
